6562651086782969f5e4c604f76795e8cf263f8a
Olivier Brückner <25103920+olivierbrcknr@users.noreply.github.com>
2025-01-05 20:26:20 +0100

    Add PWA icon

--- components/Head/index.tsx
+++ components/Head/index.tsx
@@ -7,23 +7,19 @@ const Head = ({ title }) => {
     <NextHead>
       <meta charSet="UTF-8" />
       <title>{title}</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta content="Play the card game 'Kaboom' online." name="description" />
 
-      <link
-        href="/favicon.ico"
-        rel="icon"
-        sizes="16x16 32x32"
-        type="image/x-icon"
-      />
+      <link href="/favicon.ico" rel="icon" />
+
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta content="Kaboom" name="application-name" />
+      <meta content="Kaboom" name="apple-mobile-web-app-title" />
+      <meta content="yes" name="apple-mobile-web-app-capable" />
+      <meta content="yes" name="mobile-web-app-capable" />
+      <meta content="black" name="apple-mobile-web-app-status-bar-style" />
+      <link href="/AppIcon.png" rel="apple-touch-icon" />
     </NextHead>
   );
 };
-
-// Head.propTypes = {
-//   title: string,
-//   description: string,
-//   url: string,
-//   ogImage: string,
-// };
 
 export default Head;
